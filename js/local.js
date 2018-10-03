1c347a5c7caa686e24a734ab40a7760ac0f17c81
sandeep <sandeep@ogrelogic>
2018-10-03 21:53:46 +0530

    changes after QC

--- js/local.js
+++ js/local.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $('#toggle').click(function(){
         $('.menu-area').stop().slideToggle(300);
     });
+
+    $('.menu-item-has-children').children('a').after("<i class='fa fa-angle-down drop'></i>")
+
+    $('.drop').click(function(){
+        $(this).siblings('.sub-menu').stop().slideToggle();
+    })
 });
