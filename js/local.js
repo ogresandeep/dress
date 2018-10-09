a304eeeaefdf46cc35a12374188f39aad2f3ee16
sandeep <sandeep@ogrelogic>
2018-10-09 22:00:40 +0530

    single product page complete

--- js/local.js
+++ js/local.js
@@ -28,4 +28,60 @@ $(document).ready(function(){
     /*date picker*/
     $( "#date1" ).datepicker();
 
+    /*image magnifir*/
+    $('.related-product-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
+      });
+
+
+        if (window.matchMedia("(max-width: 400px)").matches) {
+            
+           
+            
+            $('.product-large-view-area .tab-content').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots:false,
+                asNavFor: '.product-tubmb-area ul',
+                arrows:false
+            });
+            
+            $('.product-tubmb-area ul').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots:false,
+                asNavFor: '.product-large-view-area .tab-content',
+                focusOnSelect: true
+                
+            });
+
+        }
+
 });
